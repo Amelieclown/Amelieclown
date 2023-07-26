288d50eaae04b46e923308054d1c0b9f5a624624
Ramiz Nigmatulin <amelie1908@inbox.ru>
2023-07-27 00:48:36 +0300

    Создание спецификаций

---
+++ src/test/java/ramiz/backend/specification/specifications.java
@@ -0,0 +1,39 @@
+package ramiz.backend.specification;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class specifications {
+    public static RequestSpecification request(String url){
+    return new RequestSpecBuilder()
+        .setBaseUri(url)
+        .setContentType(ContentType.JSON)
+        .log(LogDetail.ALL)
+        .build();
+    }
+
+    public static ResponseSpecification response200(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .log(LogDetail.ALL)
+                .build();
+    }
+
+    public static ResponseSpecification response400(){
+        return new ResponseSpecBuilder()
+                .expectStatusCode(400)
+                .log(LogDetail.ALL)
+                .build();
+    }
+    public static void installSpec(RequestSpecification request, ResponseSpecification response){
+        RestAssured.requestSpecification = request;
+        RestAssured.responseSpecification = response;
+    }
+}
+
+
